4f6823f98635f3c4fac2e9ba7154a15f61894cd5
Vanesa <milonova.vanesa@gmail.com>
2021-11-11 17:58:26 +0100

    aplikácia aj s vendor súborom, databázou a hlavnou stránkou (pridať ikonky)

--- webpage/app/Http/Controllers/ProductController.php
+++ webpage/app/Http/Controllers/ProductController.php
@@ -2,10 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Task;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
-class TaskController extends Controller
+class ProductController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -41,10 +41,10 @@ class TaskController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Task  $task
+     * @param  \App\Models\Product  $product
      * @return \Illuminate\Http\Response
      */
-    public function show(Task $task)
+    public function show(Product $product)
     {
         //
     }
@@ -52,10 +52,10 @@ class TaskController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Models\Task  $task
+     * @param  \App\Models\Product  $product
      * @return \Illuminate\Http\Response
      */
-    public function edit(Task $task)
+    public function edit(Product $product)
     {
         //
     }
@@ -64,10 +64,10 @@ class TaskController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Task  $task
+     * @param  \App\Models\Product  $product
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Task $task)
+    public function update(Request $request, Product $product)
     {
         //
     }
@@ -75,10 +75,10 @@ class TaskController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Task  $task
+     * @param  \App\Models\Product  $product
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Task $task)
+    public function destroy(Product $product)
     {
         //
     }
