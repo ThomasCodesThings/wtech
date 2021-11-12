79a3fff99987e9c552d0eebc8ef3294928fa0434
Vanesa <milonova.vanesa@gmail.com>
2021-11-12 18:58:19 +0100

    admin časť s databázou, hotove

--- webpage/app/Http/Controllers/ProductController.php
+++ webpage/app/Http/Controllers/ProductController.php
@@ -40,19 +40,13 @@ class ProductController extends Controller
             'productTitle' => 'required|min:3',
             'productPrice' => 'required',
         ]);
-        if($request->productAmount == null){
-            $discount = false;
-        }
-        else{
-            $discount = true;
-        }
-          
+
         $task = Product::create(['productTitle' => $request->productTitle,
         'productImage' => $request->productImage, 
         'productType' => $request->productType,
         'productBrand' => $request->productBrand,
         'productAmount' => $request->productAmount,
-        'productDiscount' => $discount,
+        'productDiscount' =>  ($request->productAmount == null) ? false : true,
         'productPrice' => $request->productPrice]);
           
         /*redirect na funkciu show*/
@@ -94,13 +88,6 @@ class ProductController extends Controller
             'title' => 'required|min:3',
             'description' => 'required',
         ]);  
-
-        if($request->productAmount == null){
-            $discount = false;
-        }
-        else{
-            $discount = true;
-        }
              
         $product->productTitle = $request->productTitle ;
         $product->productImage = $request->productImage ;
@@ -108,7 +95,7 @@ class ProductController extends Controller
         $product->productPrice = $request->productPrice ;
         $product->productBrand = $request->productBrand ;
         $product->productAmount = $request->productAmount ;
-        $product->productDiscount = $discount;
+        $product->productDiscount = ($request->productAmount == null) ? false : true;
         $product->save();
         $request->session()->flash('message', 'Data succesfully changed.');
           
