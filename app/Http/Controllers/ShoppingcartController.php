a584319f5ab87ecfd80b9f4b570df839f218a41c
Vanesa <milonova.vanesa@gmail.com>
2021-11-17 18:53:59 +0100

    cart

---
+++ app/Http/Controllers/ShoppingcartController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Shoppingcart;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ShoppingcartController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Shoppingcart  $shoppingcart
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Shoppingcart $shoppingcart)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Shoppingcart  $shoppingcart
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Shoppingcart $shoppingcart)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Shoppingcart  $shoppingcart
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Shoppingcart $shoppingcart)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Shoppingcart  $shoppingcart
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Shoppingcart $shoppingcart)
+    {
+        //
+    }
+}
+
+
+/*        Shoppingcart::create(['user_id' => (Auth::check()) ? Auth::user()->id : null,
+        'ordered' => false]);
+*/
