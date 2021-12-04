860767b50860a80afa5dce869215152e9fd264e3
ThomasCodesThings <cernacek.tomas@gmail.com>
2021-12-05 00:37:20 +0100

    preklil zo vysledkov serachu

--- resources/views/pages/page/search.blade.php
+++ resources/views/pages/page/search.blade.php
@@ -6,7 +6,7 @@
 <div class="row d-flex justify-content-between overflow-fix">
 @foreach($products as $product)
 <article class="product text-center col-auto mb-3">
-                            <a class="d-block" href="#">
+                                <a class="d-block" href="{{ url('/'.$product->id) }}">
                                 <img class="product border border-secondary" src="{{ asset('resources/'.json_decode($product->productImage, true)[0]) }}">
                                 <p class="product">{{$product->productTitle}}</p>
                             </a>
@@ -16,7 +16,12 @@
                                         <p class="product text-center"><data value="50">{{$product->productPrice}}</data>$</p>
                                     </div>
                                     <div class="col">
-                                        <a href="/buy/" class="product text-center">Buy</a> 
+                                    <form action="{{ route('add-to-cart') }}" method="post">
+                                  @csrf
+                                    <input type="hidden" name="product" value="{{$product}}">
+                                    <input type="hidden" name="amount" value="1">
+                                    <a href="javascript:;" onclick="parentNode.submit();" class="product text-center">Buy</a> 
+                                    </form> 
                                     </div>
                                 </div>
                             </div>
